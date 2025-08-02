e68fa435ac5714e7619165b7ee0c86522cdac47b
mohamed abady <mohamed.abady@around25.com>
2025-08-02 19:09:10 +0300

    [GAT-02][PLUGINS]: install plugins to support static images on website

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `Thndr`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => (
   <Layout pageTitle={"Home Page"}>
     <p>great learning following gatsby documentation</p>
+    <StaticImage
+      alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+      src="../images/cuteDog.webp"
+    />
   </Layout>
 );
 
