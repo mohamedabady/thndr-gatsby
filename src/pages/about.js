09706e8f4c9613103cef67f708d03323f20c4718
mohamed abady <mohamed.abady@around25.com>
2025-08-02 18:39:06 +0300

    [GAT-01][LAYOUT]:define basic layout component

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => (
+  <Layout pageTitle="About Thndr">
+    <p>
+      here we will add all the about information about our initial site
+      thndr-gatsby
+    </p>
+  </Layout>
+);
+
+export const Head = () => <title>About Thndr</title>;
+
+export default AboutPage;
